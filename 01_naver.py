c109c9e6b34850760914592fbad7a20745fd01ae
JaeSung Byun(Mac) <bbbjason@naver.com>
2023-01-31 17:39:04 +0700

    Zip mathod for Naver

--- 01_naver.py
+++ 01_naver.py
@@ -1,22 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-address ="http://naver.com"
+base_url ="https://search.naver.com/search.naver?where=view&sm=tab_jum&query="
+keyword=input("검색어 입력하세요 : ")
+
+url =base_url + keyword
 
 window_headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 }
 
-sample_naver= requests.get(address, headers=window_headers)
+sample_naver= requests.get(url, headers=window_headers)
 
 naver_html = sample_naver.text
 soup = BeautifulSoup(naver_html, "html.parser")
-naver_logo= soup.select_one(".logo_naver").text
 
+titles = soup.select(".api_txt_lines.total_tit")
+names = soup.select(".sub_txt.sub_name")
 
+for result in zip(names,titles):
+    print(type(result))
+    print(result[0].text)
+    print(result[1].text)
+    print(result[1]['href'])
+    print()
 
-print(sample_naver.request.headers)
-
-# print(dir(sample_naver))
-# print(naver_logo) 
+# api_txt_lines total_tit _cross_trigger
 
