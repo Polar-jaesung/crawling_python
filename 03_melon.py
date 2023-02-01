42d977586ed6789c8ab3dd515462f14e399944be
JaeSung Byun(Mac) <bbbjason@naver.com>
2023-02-01 18:36:28 +0700

    add album

--- 03_melon.py
+++ 03_melon.py
@@ -18,7 +18,8 @@ list_all=list_50+list_100
 for rank,i in enumerate(list_all,1):
     title= i.select_one(".ellipsis.rank01 a")
     singer = i.select_one(".ellipsis.rank02 > a")
+    album = i.select_one(".ellipsis.rank03 > a")
     # 자식 태그 가리킬 때는 '>' 입력해주면 된다
     print(f"{rank}: {title.text}")
-    print(singer.text)
+    print(f"{singer.text} /// {album.text}")
     print()
